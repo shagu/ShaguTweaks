5c85984c69be2111b56ea19aa7068a860c65aeef
shagu <ericmauser@googlemail.com>
2021-08-24 17:15:15 +0200

    minimap-clock: add a clock to the minimap

---
+++ mods/minimap-clock.lua
@@ -0,0 +1,36 @@
+local _G = _G or getfenv(0)
+local GetExpansion = ShaguTweaks.GetExpansion
+
+if GetExpansion() ~= "vanilla" then return end
+
+local module = ShaguTweaks:register({
+  title = "Show Clock At Minimap",
+  description = "Display a 24h clock at the Minimap",
+  enabled = nil,
+})
+
+MinimapClock = CreateFrame("Frame", "Clock", Minimap)
+MinimapClock:Hide()
+MinimapClock:SetFrameStrata("MEDIUM")
+MinimapClock:SetPoint("BOTTOM", MinimapCluster, "BOTTOM", 8, 15)
+MinimapClock:SetWidth(50)
+MinimapClock:SetHeight(25)
+MinimapClock:SetBackdrop({
+  bgFile = "Interface\\Tooltips\\UI-Tooltip-Background",
+  edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border",
+  tile = true, tileSize = 8, edgeSize = 16,
+  insets = { left = 3, right = 3, top = 3, bottom = 3 }
+})
+MinimapClock:SetBackdropBorderColor(.9,.8,.5,1)
+MinimapClock:SetBackdropColor(.4,.4,.4,1)
+
+module.enable = function(self)
+  MinimapClock:Show()
+  MinimapClock.text = MinimapClock:CreateFontString("Status", "LOW", "GameFontNormal")
+  MinimapClock.text:SetFont(STANDARD_TEXT_FONT, 12, "OUTLINE")
+  MinimapClock.text:SetAllPoints(MinimapClock)
+  MinimapClock.text:SetFontObject(GameFontWhite)
+  MinimapClock:SetScript("OnUpdate", function()
+    this.text:SetText(date("%H:%M"))
+  end)
+end
