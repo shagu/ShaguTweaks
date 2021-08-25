1881b2bcab5b9d0a7802911f5af8d736a38b61a6
shagu <ericmauser@googlemail.com>
2021-08-25 09:47:10 +0200

    target-castbar: hide castbar by default

--- mods/target-castbar.lua
+++ mods/target-castbar.lua
@@ -10,12 +10,12 @@ local module = ShaguTweaks:register({
 })
 
 local castbar = CreateFrame("StatusBar", nil, TargetFrame)
-
 castbar:SetPoint("TOP", TargetFrame, "BOTTOM", 0, -20)
 castbar:SetStatusBarTexture("Interface\\TargetingFrame\\UI-StatusBar")
 castbar:SetStatusBarColor(1, .8, 0, 1)
 castbar:SetWidth(120)
 castbar:SetHeight(10)
+castbar:Hide()
 
 castbar.texture = CreateFrame("Frame", nil, castbar)
 castbar.texture:SetPoint("RIGHT", castbar, "LEFT", -4, 0)
