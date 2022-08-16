fead89455e24596a2369daab7d3e3ac22064cae2
shagu <ericmauser@googlemail.com>
2022-08-16 17:13:11 +0200

    minimap-clock: add server clock to tooltip
    also slightly adjust drawlayer and sizing of the clock

--- mods/minimap-clock.lua
+++ mods/minimap-clock.lua
@@ -10,10 +10,10 @@ local module = ShaguTweaks:register({
 
 MinimapClock = CreateFrame("Frame", "Clock", Minimap)
 MinimapClock:Hide()
-MinimapClock:SetFrameStrata("MEDIUM")
-MinimapClock:SetPoint("BOTTOM", MinimapCluster, "BOTTOM", 8, 15)
+MinimapClock:SetFrameLevel(64)
+MinimapClock:SetPoint("BOTTOM", MinimapCluster, "BOTTOM", 8, 18)
 MinimapClock:SetWidth(50)
-MinimapClock:SetHeight(25)
+MinimapClock:SetHeight(23)
 MinimapClock:SetBackdrop({
   bgFile = "Interface\\Tooltips\\UI-Tooltip-Background",
   edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border",
@@ -25,11 +25,31 @@ MinimapClock:SetBackdropColor(.4,.4,.4,1)
 
 module.enable = function(self)
   MinimapClock:Show()
+  MinimapClock:EnableMouse(true)
+
   MinimapClock.text = MinimapClock:CreateFontString("Status", "LOW", "GameFontNormal")
   MinimapClock.text:SetFont(STANDARD_TEXT_FONT, 12, "OUTLINE")
   MinimapClock.text:SetAllPoints(MinimapClock)
   MinimapClock.text:SetFontObject(GameFontWhite)
   MinimapClock:SetScript("OnUpdate", function()
     this.text:SetText(date("%H:%M"))
+  end)
+
+  MinimapClock:SetScript("OnEnter", function()
+    local h, m = GetGameTime()
+    local servertime = string.format("%.2d:%.2d", h, m)
+    local time = date("%H:%M")
+
+    GameTooltip:ClearLines()
+    GameTooltip:SetOwner(this, ANCHOR_BOTTOMLEFT)
+
+    GameTooltip:AddLine("Clock")
+    GameTooltip:AddDoubleLine("Localtime", time, 1,1,1,1,1,1)
+    GameTooltip:AddDoubleLine("Servertime", servertime, 1,1,1,1,1,1)
+    GameTooltip:Show()
+  end)
+
+  MinimapClock:SetScript("OnLeave", function()
+    GameTooltip:Hide()
   end)
 end
