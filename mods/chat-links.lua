12b9b671cde5ab652e831c69c95ed4ac1e47bb31
shagu <ericmauser@googlemail.com>
2022-11-19 15:54:33 +0100

    chat-links: only hook existing chat windows

--- mods/chat-links.lua
+++ mods/chat-links.lua
@@ -182,7 +182,7 @@ module.enable = function(self)
 
   do -- add class colors to chat
     for i=1,NUM_CHAT_WINDOWS do
-      if not _G["ChatFrame"..i].HookAddMessage then
+      if _G["ChatFrame"..i] and not _G["ChatFrame"..i].HookAddMessage then
         _G["ChatFrame"..i].HookAddMessage = _G["ChatFrame"..i].AddMessage
         _G["ChatFrame"..i].AddMessage = function(frame, text, a1, a2, a3, a4, a5)
           if text then
